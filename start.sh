ffc994914416dc571f0a0821e854804c95a7b810
SuvashSoren <122045341+SuvashSoren@users.noreply.github.com>
2023-01-05 23:13:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/RoyalSubas/MdiskSearchBot.git /MdiskSearchBot
-cd /MdiskSearchBot
+git clone https://github.com/SuvashSoren/MdiskSearchVBot.git /MdiskSearchVBot
+cd /MdiskSearchVBot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
